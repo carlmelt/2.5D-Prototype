8a310de4752356006790a5e0cf7df09f027fdc95
Rio Saputro <rio.ae23@gmail.com>
2024-03-01 18:22:43 +0700

    Character Container

---
+++ Assets/Scripts/Characters/CharacterContainer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterContainer : ScriptableObject
+{
+    // public string Name;
+    //public Character character; -> Model, Animasi
+    // public CharacterStats stats;
+    //public  Weapon characterWeapon;
+    // public SkillHolder characterSkill1;
+    // public SkillHolder characterSkill2;
+    // public SkillHolder characterUltimateSkill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
