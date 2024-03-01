069a6d1e80b6e50b1158b64d331e91f4f480da82
Rio Saputro <rio.ae23@gmail.com>
2024-03-01 20:19:17 +0700

    Skill and change Scene Testing

---
+++ Assets/Scripts/UI/UIButtons.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour
+{
+  
+  public void Startbtn(){
+    SceneManager.LoadScene("Skill");
+  }
+    public void SkillBtn(){
+       SceneManager.LoadScene("Menu");
+    }
+}
