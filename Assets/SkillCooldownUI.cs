28ad03593b2b12e4aa1a65230a31eabedac37b8c
pesal <faishalsyahbari@gmail.com>
2024-03-02 13:52:26 +0700

    Added Cooldown UI. It works!

--- Assets/SkillCooldownUI.cs
+++ Assets/SkillCooldownUI.cs
@@ -1,20 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillCooldownUI : MonoBehaviour
 {
+    public PlayerSkill playerSkill;
+    Button pressedButton;
+    public List<BaseSkill> skills;
+    public List<Button> skillButtons = new List<Button>{};
 
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        playerSkill = FindObjectOfType<PlayerSkill>();
+        playerSkill.StartCooldown += (cd, skill) => StartCoroutine(OverlayCooldown(cd, skill));
+        foreach (Transform child in transform){
+            skillButtons.Add(child.GetComponent<Button>());
+        }
+        PlayerMovement player = playerSkill.GetComponent<PlayerMovement>();
+        skills = new List<BaseSkill> {player.playerContainer.skill1, player.playerContainer.skill2, player.playerContainer.ultimateSkill};
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnDisable(){
+        playerSkill.StartCooldown -= (cd, skill) => StartCoroutine(OverlayCooldown(cd, skill));
+    }
+
+    // public void ButtonUpdate(Button button){
+    //     pressedButton = button;
+    // }
+
+    IEnumerator OverlayCooldown(float cooldown, BaseSkill skillUsed){
+        float durationLeft = cooldown;
+        Image CDOverlay = skillButtons[skills.IndexOf(skillUsed)].transform.GetChild(1).gameObject.GetComponent<Image>();
+        while (durationLeft > 0){
+            durationLeft -= Time.fixedDeltaTime;
+            durationLeft = Mathf.Max(0, durationLeft);
+            Debug.Log(durationLeft);
+            CDOverlay.fillAmount = durationLeft / cooldown;
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
