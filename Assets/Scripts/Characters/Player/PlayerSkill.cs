3afc9ce03204e0fa9b9ca3655fd70418318139b1
Rio Saputro <rio.ae23@gmail.com>
2024-03-02 12:03:49 +0700

    Adding second skill button, Enhancing Cooldown System

--- Assets/Scripts/Characters/Player/PlayerSkill.cs
+++ Assets/Scripts/Characters/Player/PlayerSkill.cs
@@ -9,6 +9,7 @@ public class PlayerSkill : MonoBehaviour
     public event Action<float> StartCooldown = delegate {};
     public Animator playerAnimator;
     public bool canSkill = true;
+    [SerializeField] private float currentCooldown;
     public float skillCooldown;
     public Transform skillSpawnPoint;
     AnimatorOverrideController animatorOverrider;
@@ -22,26 +23,55 @@ public class PlayerSkill : MonoBehaviour
         }
     }
     public void Skill(BaseSkill skillToUse){
-        if (canSkill && !playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("SkillCast")){
+        if(skillToUse.isCooldown) return;
+
+
+        if (!playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("SkillCast")){
             skillCooldown = skillToUse.cooldown;
+            currentCooldown = skillCooldown;
             animatorOverrider["TestFireSlash"] = skillToUse.skillAnim; //The default skill anim is TestFireSlash
             // currentAnimation = skill.skillAnim;
             // Debug.Log(animatorOverrider.clips);
             // Debug.Log(currentAnimation);
             skillToUse.Activate(this);
-            canSkill = false;
+            skillToUse.isCooldown = true;
             playerAnimator.SetTrigger("Skill");
             // GameObject particleGO = Instantiate(skillEffect, skillSpawnPoint.position, skillSpawnPoint.rotation);
             // Destroy(particleGO, 2f);
             SkillCasted.Invoke(skillToUse.castTime);
-            StartCoroutine(SkillCooldown(skillCooldown));
+            StartCoroutine(SkillCooldown(skillCooldown, skillToUse));
+            // checkSkillCooldown(skillToUse);
         }
     }
 
-    IEnumerator SkillCooldown(float Cd)
+    IEnumerator SkillCooldown(float Cd, BaseSkill skill)
     {
-        yield return new WaitForSeconds(Cd);
-        canSkill = true;
+        float remainingTime = Cd;
+        while (remainingTime > 0)
+        {
+            Debug.Log("Remaining Time: " + remainingTime);
+            remainingTime -= Time.fixedDeltaTime;
+            Debug.Log("Skill is on cooldown for " + remainingTime + " seconds");
+            yield return new WaitForFixedUpdate();
+        }
+        // yield return null;
+        // StartCooldown.Invoke(Cd);
+        skill.isCooldown = false;
         Debug.Log("Skill Ready");
+    }
+
+    public void checkSkillCooldown(BaseSkill skill){
+        if (!skill.isCooldown) return;
+        else {
+            float cd = currentCooldown;
+            cd -= Time.fixedDeltaTime;
+            currentCooldown = cd;
+            Debug.Log("Skill is on cooldown for " + cd + " seconds");
+            // while(Time.time - cd > skill.cooldown) {
+            //     cd -= Time.fixedDeltaTime;
+            // }
+            // cd -= Time.fixedDeltaTime;
+            // Debug.Log("Skill is on cooldown for " + cd + " seconds");
+        }
     }
 }
